6b9f43c8ed5f65d5fbfd6144b32cae18eb39de81
arinalhak <93799643+arinalhak@users.noreply.github.com>
2022-03-04 14:32:07 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'mydb',
+	'hostname' => '108.136.222.169',
 	'username' => 'arinal',
 	'password' => 'arin4l',
 	'database' => 'usermanagementdb',
